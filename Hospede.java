2cfef89e768d11f220aa7c7432638d1c22bfdeb3
Lisxt <alice14072005@gmail.com>
2024-09-25 20:48:47 -0300

    Update Hospede.java

--- Hospede.java
+++ Hospede.java
@@ -1,47 +1,38 @@
 package Reserva;
-
-
-
 public class Hospede {
-//atributos 
-	private String nome;
-	private String cpf;
-	private int nascimento;
+		//atributos 
+			private String nome;
+			private String cpf;
+			private int nascimento;
 
-//contrutores
-	
-	public Hospede (String nome) {
-		this.nome = nome;
-	}
-	
-	public Hospede (String nome, String cpf, int nascimento) {
-		this.nome = nome;
-		this.cpf = cpf;
-		this.nascimento = nascimento;
-	}
-		
-//metodos de acesso
-	//get - permitir que outras partes do código leiam o valor da variável sem permitir alterações diretas
+		//contrutores
+			
+			public Hospede (String nome) {
+				this.nome = nome;
+			}
+			
+			public Hospede (String nome, String cpf, int nascimento) {
+				this.nome = nome;
+				this.cpf = cpf;
+				this.nascimento = nascimento;
+			}
+				
+		//metodos de acesso
+			//get - permitir que outras partes do código leiam o valor da variável sem permitir alterações diretas
 
-	public String getNome() {
-		return nome;
-	}
-	public String getCpf() {
-		return cpf;
-	}
-	public int getData() {
-		return nascimento;
-	}
+			public String getNome() {
+				return nome;
+			}
+			public String getCpf() {
+				return cpf;
+			}
+			public int getData() {
+				return nascimento;
+			}
 
-//metodos
-	public boolean verificarMaiorIdade() {
-		int idade = 2024 - nascimento;
-		return idade > 18;
-}
-//	public void exibir() {
-//		System.out.println("Nome: " + nome);
-//		System.out.println("Idade: " + cpf);
-//		System.out.println("Nascimento: " + nascimento);
-//		verificarMaiorIdade();
-//	}
+		//metodos
+			public boolean verificarMaiorIdade() {
+				int idade = 2024 - nascimento;
+				return idade > 18;
+		}
 }
